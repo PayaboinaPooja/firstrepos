e70287802589fbfa994ffbc8b20589c3d0fa0677
PayaboinaPooja <62057830+PayaboinaPooja@users.noreply.github.com>
2021-02-06 14:22:31 +0530

    Add files via upload

---
+++ Avgnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Avgnum
+    {
+        static void Main(string[] args)
+        {
+            int num = 234;
+            int sum = 0;
+            int count = 0;
+            int avg = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum = sum + digit;
+                count++;
+                num = num / 10;
+            }
+            avg = sum / count;
+            Console.WriteLine(avg);
+        }
+    }
+}
